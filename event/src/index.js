ab6e3fd6ebcc68c38f573594030df46773d8f161
adelavegaf <antoniodlavga@gmail.com>
2017-10-02 19:46:26 -0500

    Use local storage to store redux state.

--- event/src/index.js
+++ event/src/index.js
@@ -4,22 +4,46 @@ import {alias, wrapStore} from 'react-chrome-redux';
 import aliases from './utils/aliases';
 import thunk from 'redux-thunk';
 import Api from './utils/api';
+import {throttle} from 'lodash';
 
-const store = createStore(rootReducer, applyMiddleware(
-    alias(aliases),
-    thunk
-));
+chrome.storage.local.get(['state'], ({state}) => {
+    const store = createStore(
+        rootReducer,
+        state,
+        applyMiddleware(
+            alias(aliases),
+            thunk
+        )
+    );
 
-wrapStore(store, {
-    portName: 'browsing-stats'
-});
+    wrapStore(store, {
+        portName: 'browsing-stats'
+    });
 
-Api.onLogIn(() => {
-    store.dispatch({type: 'SET_AUTHENTICATED_STATUS', authenticated: true});
-    store.dispatch({type: 'SET_AUTHENTICATION_LOADING', authenticationLoading: false});
-});
+    /**
+     * Save the current store state to local storage
+     */
+    const saveState = () => {
+        console.info('Saving state to chrome.storage.local');
 
-Api.onLogOut(() => {
-    store.dispatch({type: 'SET_AUTHENTICATED_STATUS', authenticated: false});
-    store.dispatch({type: 'SET_AUTHENTICATION_LOADING', authenticationLoading: false});
+        const state = store.getState();
+
+        chrome.storage.local.set({
+            state: state
+        });
+    };
+
+    // On new state, persist to local storage
+    const throttledSave = throttle(saveState, 5000, {trailing: true, leading: true});
+    store.subscribe(throttledSave);
+
+    Api.onLogIn(() => {
+        store.dispatch({type: 'SET_AUTHENTICATED_STATUS', authenticated: true});
+        store.dispatch({type: 'SET_AUTHENTICATION_LOADING', authenticationLoading: false});
+    });
+
+    Api.onLogOut(() => {
+        store.dispatch({type: 'SET_AUTHENTICATED_STATUS', authenticated: false});
+        store.dispatch({type: 'SET_AUTHENTICATION_LOADING', authenticationLoading: false});
+    });
 });

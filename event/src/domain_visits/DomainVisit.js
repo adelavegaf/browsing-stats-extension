834ba3c2f5f3da5baf5f29a0ad290df43386c7f1
adelavegaf <antoniodlavga@gmail.com>
2017-10-21 17:04:32 -0500

    Bug fix: extension now has a persistent background page to track time correctly.

--- event/src/domain_visits/DomainVisit.js
+++ event/src/domain_visits/DomainVisit.js
@@ -5,33 +5,27 @@ export default class DomainVisit {
         this.id = null;
         this.hostname = hostname;
         this.lastUpdateDate = new Date();
-        this.timeSpent = 0;
-
-        Api.addVisit(this.hostname, this.timeSpent)
-           .then(id => {
-               this.id = id;
-           })
-           .catch(error => {
-               console.error(error);
-           });
+        this.saveToDBPromise = Api.addVisit(this.hostname, 0 /* timeSpent */)
+                                  .then(id => {
+                                      this.id = id;
+                                  })
+                                  .catch(error => {
+                                      console.error(error);
+                                  });
     }
 
     increaseTimeSpent() {
-        /**
-         * Check if the database insert has already fulfilled.
-         */
-        if (!this.id) {
-            return;
-        }
         const currentTime = new Date();
-        this.timeSpent = currentTime - this.lastUpdateDate;
+        const timeSpent = currentTime - this.lastUpdateDate;
         this.lastUpdateDate = currentTime;
-        Api.increaseTimeSpentOnVisit(this.id, this.timeSpent)
-           .then(() => {
-               console.info('time spent on', this.hostname, 'was updated by', this.timeSpent, 'ms');
-           })
-           .catch(error => {
-               console.error(error);
-           });
+        this.saveToDBPromise.then(() => {
+            Api.increaseTimeSpentOnVisit(this.id, timeSpent)
+               .then(() => {
+                   console.info('time spent on', this.hostname, 'was updated by', timeSpent, 'ms');
+               })
+               .catch(error => {
+                   console.error(error);
+               });
+        });
     }
 }
